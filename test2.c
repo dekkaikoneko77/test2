b9f1c48b36097c5dd394be912d530f55075acbcc
dekkaikoneko <iaki83998@gmail.com>
2026-09-25 14:31:39 +0900

    testだよ　ここに何打てばよいかわからん

---
+++ test2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    if (x % 3 == 0) {
+        printf("fizz\n");
+    } else if (x % 5 == 0) {
+        printf("buzz\n");
+    } else {
+        printf("%d\n", x);
+    }
+    return 0;
+}
